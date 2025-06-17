764e37cff273d0efe1a654f9811f8e58c02343ac
JC Glow Jewerly <jcglow.jewerly@gmail.com>
2025-06-16 22:53:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -118,6 +118,15 @@ function addToCart(product) {
   }
   saveCart();
   updateCartUI();
+  document.getElementById("clear-cart").addEventListener("click", () => {
+  if (confirm("¿Estás segura de que deseas vaciar todo el carrito?")) {
+    cart = {};
+    localStorage.removeItem("jcglowCart");
+    updateCartUI();
+    hideCart();
+  }
+});
+
   showCart();
 }
 
